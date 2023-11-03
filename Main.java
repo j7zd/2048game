322a4ed2978b66028f6ddcc567427f05f6e823c5
j7zd <x8bd7@proton.me>
2023-11-03 12:31:13 +0200

    small changes to Main

--- Main.java
+++ Main.java
@@ -1,8 +1,10 @@
-import game2048.ConsoleUserInterface;
+//import game2048.ConsoleUserInterface;
 import game2048.VisualUserInterface;
 
 public class Main {
     public static void main(String[] args) {
+        // ConsoleUserInterface ui = new ConsoleUserInterface();
+        // ui.startGame();
         VisualUserInterface ui = new VisualUserInterface();
         ui.startGame();
     }
